15362b0ca1cf1b22ab341b82835103d7d3d8ca50
rosayesses24-arch <rosayesses_24@alu.uabcs.mx>
2026-03-04 18:09:15 -0600

    menú

--- src/practica/Ventana.java
+++ src/practica/Ventana.java
@@ -10,10 +10,15 @@ import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextArea;
 
 public class Ventana extends JFrame {
@@ -34,8 +39,53 @@ public class Ventana extends JFrame {
         //this.setBackground(Color.blue);
       	//this.setOpacity(1);
         this.getContentPane().setBackground(Color.pink);
+        
+        JMenuBar barra = new JMenuBar();
+        this.setJMenuBar(barra);
+        
+        JMenu menu1 = new JMenu("Archivo");
+        barra.add(menu1);
+        
+        JMenuItem opt1_mi = new JMenuItem("Abrir");
+        menu1.add(opt1_mi);
+        
+        JMenuItem opt2_mi = new JMenuItem("Nuevo");
+        menu1.add(opt2_mi);
+        
+        JMenuItem opt3_mi = new JMenuItem("Cerrar");
+        menu1.add(opt3_mi);
+        
+        menu1.addSeparator();
+        
+        JMenu menu2 = new JMenu("Guardar");
+        menu1.add(menu2);
+        
+        JMenuItem opt4_mi = new JMenuItem("Guardar");
+        menu2.add(opt4_mi);
+        
+        JMenuItem opt5_mi = new JMenuItem("Guardar como");
+        menu2.add(opt5_mi);
+        
+       
+      
+        
+        
+        
+        
+     
+        
+        
+        //this.login();
+        //this.registro();
+       this.setVisible(true);
+        
+        this.users();
+        this.repaint();
+    
+    }
 
-        JPanel login_container = new JPanel();
+       public void login() {
+       JPanel login_container = new JPanel();
         login_container.setBounds(70,100,400,600);
         login_container.setBackground(Color.LIGHT_GRAY);
         login_container.setLayout(null);
@@ -92,6 +142,8 @@ public class Ventana extends JFrame {
         access_btn.setFont(new Font("Segoe UI", Font.PLAIN, 22));
         login_container.add(access_btn);
         
+       }
+       public void registro() {
         //panel  
         JPanel register_container= new JPanel();
         register_container.setBounds(530,100,400,600);
@@ -214,24 +266,45 @@ public class Ventana extends JFrame {
          register_btn.setForeground(Color.BLACK);
          register_btn.setBounds(50, 530, 300, 45);
         register_container.add(register_btn);
-         
-         
-       
-       
-       
-       
-       
-       
-       
-        
-       
-        
-        
-        
-        
-        
-        
-        this.repaint();
+    	
+       }
+    	public void users() {
+    		JPanel panel_users = new JPanel();
+    		panel_users.setBounds(50, 50, 850, 550);
+    		panel_users.setOpaque(true);
+    		//panel_users.setBackground(Color.white);
+    		//panel_users.setBorder(BorderFactory.createLineBorder(Color.decode("#18272b"), 4, true));
+    		panel_users.setLayout(null);
+    		this.add(panel_users);
+    		
+    		String [] table_head = {"No. Control", "Nombre", "Apellidos", "Correo electrónico", "Semestre", "Carrera", "Acciones"};
+    		
+    		Object [][] table_body = {
+    			    {"20451234", "Luis", "Hernández", "luis.hdz@example.com", "3", "Ingeniería en Sistemas", "Acciones"},
+    			    {"20457891", "María", "Gómez", "maria.gmz@example.com", "5", "Arquitectura", "Acciones"},
+    			    {"20453422", "Jorge", "Ramírez", "jorge.rz@example.com", "2", "Contaduría", "Acciones"},
+    			    {"20458977", "Ana", "López", "ana.lp@example.com", "7", "Administración", "Acciones"},
+    			    {"20452311", "Carlos", "Martínez", "carlos.mtz@example.com", "4", "Ingeniería Civil", "Acciones"},
+    			    {"20455678", "Fernanda", "Soto", "fer.soto@example.com", "6", "Diseño Gráfico", "Acciones"},
+    			    {"20459832", "Ricardo", "Pérez", "ricardo.pz@example.com", "1", "Ingeniería Mecánica", "Acciones"},
+    			    {"20454490", "Daniela", "Vargas", "daniela.vg@example.com", "8", "Psicología", "Acciones"},
+    			    {"20457701", "Héctor", "Castro", "hector.cs@example.com", "9", "Derecho", "Acciones"},
+    			    {"20456642", "Sofía", "Navarro", "sofia.nv@example.com", "3", "Medicina", "Acciones"},
+    			    {"20453387", "Miguel", "Ortega", "miguel.og@example.com", "10", "Ingeniería Industrial", "Acciones"},
+    			    {"20451298", "Paola", "Reyes", "paola.ry@example.com", "2", "Mercadotecnia", "Acciones"}
+    		};
+    		
+    		
+    		JTable students = new JTable(table_body,table_head);
+    		
+    		JScrollPane final_table = new JScrollPane(students);
+    		final_table.setSize(700, 150);
+    		final_table.setLocation(100, 100);
+    		
+    		panel_users.add(final_table);
+    		
+    
+              
     }
 
     public static void main(String[] args) {
